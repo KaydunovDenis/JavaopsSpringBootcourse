6619f061e8ccd9aa94050d3287ca61228c1559a7
Denis Kaydunov <deniskaydunov@sap.com>
2023-12-15 11:17:49 +0300

    initial commit

---
+++ src/main/java/com/github/kaydunovdenis/javaopsspringbootcourse/JavaopsSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaydunovdenis.javaopsspringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaopsSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaopsSpringBootCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kaydunovdenis/javaopsspringbootcourse/JavaopsSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kaydunovdenis.javaopsspringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaopsSpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
